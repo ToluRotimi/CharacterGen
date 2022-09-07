792356acbde7af6a5cfbac2a7c224961322b0cd6
Tolu Rotimi <trotimi22@gmail.com>
2022-09-07 16:13:40 +0000

    testing

---
+++ back-end/character-api/tests/test_character_api.py
@@ -0,0 +1,16 @@
+from flask import url_for
+from flask_testing import TestCase
+import pytest
+
+from application import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestViews(TestBase):
+
+    def test_get_character(self):
+        response = self.client.post(url_for('character'), json={"Colour": "Red", "Personality":"Anticipative""Brave""Curious"})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Character', response.data)
